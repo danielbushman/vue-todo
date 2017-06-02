f1f57d7a6f9028343c7327490b66c3640082c704
Daniel Bushman <daniel.bushman@singularity.software>
2017-06-01 22:59:02 -0600

    save

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,12 +1,13 @@
 <template lang="pug">
   b-input-group
-    b-form-checkbox(v-model="completed" value="yes" unchecked-value="no")
+    b-form-checkbox(v-model="todo.completed" value="yes" unchecked-value="no")
     b-form-input(v-model="action")
 </template>
 
 <script>
   export default {
     name: 'task',
+    props: 'task',
     data () {
       return {
         completed: 'no',
